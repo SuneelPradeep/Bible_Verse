643f9814fcc775ce70fff0337675b27370f4ee68
SuneelPradeep <ernestsolomoninzion@gmail.com>
2025-05-04 18:37:15 +0530

    netlify @ changes to ../

--- app/api/getquote/_app.tsx
+++ app/api/getquote/_app.tsx
@@ -1,9 +1,9 @@
 import { Amplify } from 'aws-amplify'
 import type { AppProps } from 'next/app'
 import '../../../app/globals.css'
-import awsExports from '../../../src/aws-exports'
+import awsmobile from '../../../src/aws-exports'
 
-Amplify.configure({...awsExports})
+Amplify.configure({...awsmobile})
 //ssr : true not added as its showing error 
 export default function App ({ Component, pageProps} : AppProps) {
     return <Component {...pageProps} />
